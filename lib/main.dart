7f0527d80eb2bd17a9f9383f0abe0ded9845e35e
mahfuj-ahmed-jim <mahfujahmedjim32@gmail.com>
2023-09-30 00:51:20 +0600

    Init

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Netflix UI',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
